6efc21193aea7b0d541061a1a5d2cd51445268a8
H-Habka <hossien.habka1@gmail.com>
2022-02-14 07:48:37 +0200

    router is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homePage.component';
 
+const Hatspage = () =>(
+  <div>
+    <h1>HATS</h1>
+  </div>
+)
+const Womenspage = () =>(
+  <div>
+    <h1>womens</h1>
+  </div>
+)
+const Menspage = () =>(
+  <div>
+    <h1>Mens</h1>
+  </div>
+)
+const Sneakerspage = () =>(
+  <div>
+    <h1>sneakers</h1>
+  </div>
+)
+const Jacketspage = () =>(
+  <div>
+    <h1>jackets</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/shop/hats"} element={<Hatspage />} />
+        <Route path={"shop/jackets"} element={<Jacketspage />} />
+        <Route path={"shop/sneakers"} element={<Sneakerspage />} />
+        <Route path={"shop/mens"} element={<Menspage />} />
+        <Route path={"shop/womens"} element={<Womenspage />} />
+      </Routes>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './menu-item.style.scss';
+import { Link } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`} >
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+    <Link to={linkUrl} className={`${size} menu-item`}>
         <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className="background-image"/>
-        <div className= "content">
+            backgroundImage: `url(${imageUrl})`
+        }} className="background-image" />
+        <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle"> shop now</span>
         </div>
-    </div>
+    </Link>
 )
 
 
